b51305a2b33f4a48cd422c9492c18efe842a0847
CaduCarfi <157553755+CaduCarfi@users.noreply.github.com>
2026-03-12 22:26:44 -0300

    Finalização Controller e Service

--- crud/src/main/java/com/crud_springboot/crud/controller/ProdutoController.java
+++ crud/src/main/java/com/crud_springboot/crud/controller/ProdutoController.java
@@ -1,4 +1,47 @@
 package com.crud_springboot.crud.controller;
 
+import com.crud_springboot.crud.model.ProdutoModel;
+import com.crud_springboot.crud.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping(path = "/produto")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoModel>> findAll(){
+        List<ProdutoModel> requeste = produtoService.findAll();
+        return ResponseEntity.ok().body(requeste);
+    }
+
+    @PostMapping
+    public ResponseEntity <ProdutoModel> criarProduto(@RequestBody ProdutoModel produtoModel){
+        return  ResponseEntity.status(204).body(produtoModel);
+    }
+
+    @PutMapping
+    public ResponseEntity<ProdutoModel> atualizarProduto(@RequestBody ProdutoModel produtoModel) {
+        return ResponseEntity.status(204).body(produtoModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletar (@PathVariable Long id){
+        produtoService.deletarProduto(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoModel> buscarId(@PathVariable Long id){
+        ProdutoModel produto = produtoService.buscarId(id);
+        return ResponseEntity.ok(produto);
+    }
 }
--- crud/src/main/java/com/crud_springboot/crud/service/ProdutoService.java
+++ crud/src/main/java/com/crud_springboot/crud/service/ProdutoService.java
@@ -1,4 +1,41 @@
 package com.crud_springboot.crud.service;
 
+import com.crud_springboot.crud.model.ProdutoModel;
+import com.crud_springboot.crud.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<ProdutoModel> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    public void deletarProduto(Long id) {
+        produtoRepository.deleteById(id);
+    }
+
+    public ProdutoModel buscarId(Long id) {
+        return produtoRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Não encontrado"));
+    }
+
+    public ProdutoModel cadastrarProduto(ProdutoModel produtoModel) {
+        return produtoRepository.save(produtoModel);
+    }
+
+    public ProdutoModel atualizarProduto(Long id, ProdutoModel produtoModel) {
+        ProdutoModel produto = produtoRepository.findById(id).get();
+        produto.setNome(produtoModel.getNome());
+        produto.setPreco(produtoModel.getPreco());
+        return produtoRepository.save(produto);
+    }
+
 }
